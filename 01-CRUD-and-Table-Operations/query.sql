8f0924fae9cc5c20ceb876fb5b0ce6553f075c83
Arghya Chowdhury <arghachowdhury2006@gmail.com>
2026-09-13 01:40:35 +0530

    Implement CRUD operations for person table
    Added SQL commands for CRUD operations on the person table.

--- 01-CRUD-and-Table-Operations/query.sql
+++ 01-CRUD-and-Table-Operations/query.sql
@@ -1,22 +1,27 @@
+--CREATE TABLE--
 CREATE TABLE person(
 id INT,
 name VARCHAR(100),
 city VARCHAR(100)
 );
 
+--INSERT VALUES--
 INSERT INTO person(id, name, city)
 VALUES
 (101, 'Arghya', 'West Bengal'),
 (103, 'Suman', 'Delhi'),
 (102, 'Ram', 'Kolkata');
 
+--READ VALUES--
 SELECT * FROM person;
 SELECT name, id FROM person;
 
+--UPDATE VALUES--
 UPDATE person
 SET city = 'Mumbai'
 WHERE name = 'Arghya';
 
+--DELETE SPECIFIC VALUES--
 DELETE FROM person
 WHERE id = 103;
 
